7f1248f4943266d842b6ba73522ec098571ed322
Chinnatip Taemkaeo <chinnatip@me.com>
2019-05-11 17:35:26 +0700

    first commit map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Map from './components/Map'
+
+const INIT_LOCATION = {
+  from: 'Neo4Map',
+  fromTerminal: {
+    coordinate: {
+      lat: '13.740596',
+      lng: '100.562926'
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Map
+          collection={INIT_LOCATION}
+          height="768px"
+          width="100%"
+          zoom="6"
+          disableUI
+          markerable
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
